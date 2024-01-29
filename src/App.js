ff5cb918bdf9332594e9c6ec460e695cba976226
Nighteast <jung946244@naver.com>
2024-01-29 13:56:22 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="nav">
+        <div>
+          <h1>navbar</h1>
+        </div>
+      </div>
+      <div className="home">
+        <h1>ㅎㅇ</h1>
+        <h2>ㅎㅇ2</h2>
+      </div>
+      <div className="about">
+        <h1>About</h1>
+      </div>
+      <div className="skill">
+        <h1>Skill</h1>
+      </div>
+      <div className="project">
+        <h1>project</h1>
+      </div>
+      <div className="footer">
+        <h1>footer</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
